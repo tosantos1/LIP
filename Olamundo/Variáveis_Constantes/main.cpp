781155230977dec117bcb594aeac01c9ecea4525
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-22 15:50:29 -0300

    Variáveis e Constante

---
+++ Olamundo/Variáveis_Constantes/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x = 45;
+
+   cout <<"Informe um valor: " << x << endl;
+
+   x = 10;
+
+   cout << "Valor: " << x << endl;
+
+}
