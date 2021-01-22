c0c52b3aaf30bf74c0d92cee94b16254f45852e5
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-22 15:38:35 -0300

    c++

---
+++ Olamundo/main/main float int char.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float var;
+    int inteiro;
+
+    cout << "Informe um caractere: ";
+
+    cin >> var;
+
+    cout << "valor lido " << var << endl;
+
+    inteiro = var;
+
+    cout << "inteiro = " << inteiro << endl;
+    return 0;
+}
