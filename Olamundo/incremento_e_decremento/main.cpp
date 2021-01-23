a0d84fe012ab462d76e8be8b30871c88f92b5471
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-23 19:06:01 -0300

    Incremento_e_decremento

---
+++ Olamundo/incremento_e_decremento/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a = 2;
+   int b = 3;
+   int c;
+
+   cout << "a = " << a << ", b = " << b << endl;
+
+   c = --a * b;
+
+   cout << "c = " << c << endl;
+
+    return 0;
+}
