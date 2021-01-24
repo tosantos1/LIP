16dab5a664e974437ac450f5c7ec6317421387d2
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-24 15:22:25 -0300

    ifs_exercícios

--- Olamundo/ifs_exercicios/main.cpp
+++ Olamundo/ifs_exercicios/main.cpp
@@ -14,11 +14,20 @@ int main()
 
     if (delta < 0)
     {
-        cout << "Equacao nao possui raizes" << end;
+        cout << "Equacao nao possui raizes reais" << endl;
     }
     else
     {
+        if(delta == 0){
+            x1 = x2 = -b /(2*a);
 
+            cout << "raiz: " << x1 << endl;
+        }
+        else{
+            x1 = (- b + sqrt(delta))/(2*a);
+            x2 = (- b - sqrt(delta))/(2*a);
+            cout << "x1 = " << x1 << ", x2 = " << x2 << endl;
+        }
     }
     return 0;
 }
