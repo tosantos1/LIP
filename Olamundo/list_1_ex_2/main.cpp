3826a3f4530f30307e825ebfc6538dd540ff419f
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-25 23:31:23 -0300

    Lista 1 ex 2

---
+++ Olamundo/list_1_ex_2/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/*Notas: Implemente um programa que lê três notas de um aluno em uma disciplina
+ e imprime a situação do aluno conforme as regras da UFRN, ou seja:
+ - Aprovado: média das notas é maior ou igual a 5 e o aluno não tirou nenhuma nota abaixo de 3
+ - Final: caso o aluno não tenha sido aprovado e ele possua média maior ou igual a 3
+ - Reprovado: caso a média do aluno seja menor do que 3*/
+
+int main()
+{
+    float nota1, nota2, nota3, media;
+    float aprovado, final, reprovado;
+
+    cin >> nota1;
+    cin >> nota2;
+    cin >> nota3;
+
+    media = (nota1 + nota2 + nota3)/3;
+
+    if(nota1 >= 3 && nota2 >= 3 && nota3 >= 3 ){
+        if(media >= 5){
+        cout << "Aprovado" << media << endl;
+    }if(media < 5 || nota1 < 3 || nota2 < 3 || nota3 < 3 ){
+                cout <<"Final" << media << endl;
+        }
+
+    }
+    return 0;
+}
