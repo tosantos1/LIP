677d7dc462c6156a8e4441e53450965a6d190a81
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-25 21:43:34 -0300

    Operador Ternário

---
+++ Olamundo/operador_ternario/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+/*int x, y;
+cin >> x;
+ if(x>10)
+       {
+           y = x +3;
+       }else {
+            y = 3*x;
+       }*/
+int x, y;
+cin >> x;
+y = x > 10? x+3 : x*3;
+cout << "y" << y << endl;
+           return 0;
+}
