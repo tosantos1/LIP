e6dd36f6457e150b289a11d28ce8eb038d9cb729
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-27 17:35:04 -0300

    Exercicio Presença Lab

---
+++ Olamundo/exer/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstring>
+using namespace std;
+int main() {
+    int x, y, z, t;
+    cin >> x >> y >> z;
+    cout << t;
+    return 0;
+}
