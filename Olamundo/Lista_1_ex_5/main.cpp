88f32f5c2446daadb4104bdc8e69483c34cc66b1
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-26 21:12:06 -0300

    Lista 1 Ex 2

---
+++ Olamundo/Lista_1_ex_5/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*Escreva um programa que lê três valores inteiros, que correspodem, respectivamente,
+aos coeficientes a, b e c, de uma equação do segundo grau a*x^2 + b*x + ca∗x
+2
+ +b∗x+c. O seu programa deve calcular e mostrar as raízes dessa equação, de acordo com os seguintes critérios:
+
+Imprima a mensagem Sem raiz real, caso não existam raízes reais.
+Se a equação possui uma única raiz, imprima ela somente uma vez.
+Se a equação possui duas raízes, imprima primero a raiz de menor valor.*/
+
+
+int main()
+{
+    int a, b, c, delta;
+    int x1, x2;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    delta = (b*b)-(4*a*c);
+
+    if (delta < 0){
+        cout << "Sem raiz real" << endl;
+    }else{
+
+        x1 = (-b + sqrt(delta))/2*a;
+        x2 = (-b - sqrt(delta))/2*a;
+
+        if(x1 == x2){
+
+            cout << "" << x1 << endl;
+        }else {
+
+            cout << "" << x1 << "" << x2 << endl;
+        }
+
+    }
+
+
+    return 0;
+}
---
+++ Olamundo/Lista_1_ex_6/main.cpp
@@ -0,0 +1,13 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float d, kml=12, dpl;
+    cin >> d;
+    dpl = d/kml;
+    cout << fixed << setprecision(2);
+    cout << dpl << endl;
+    return 0;
+}*/
