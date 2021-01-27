83bcbaf551fc6652461543a60615098c734756b4
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-27 19:26:04 -0300

    Exercicio presença

--- Olamundo/exer/main.cpp
+++ Olamundo/exer/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
-#include <cstring>
+
 using namespace std;
-int main() {
-    int x, y, z, t;
-    cin >> x >> y >> z;
-    cout << t;
+
+
+
+int main()
+{
+    int x, y, menor, maior, k=0;
+
+    for (int i = 0; i<10; i++){
+        cin >> x;
+    }
+    y = x[0];
+    while(k < 10){
+
+        i++;
+        if(y<x[k]){
+            menor = y;
+        }else {
+            y = x[k];
+        }
+        if(y>x[k]){
+            maior = y;
+        }else {
+            y = x[k];
+        }
+    }
+    cout << " " << maior << " " << menor << endl;
+
+
+
     return 0;
 }
