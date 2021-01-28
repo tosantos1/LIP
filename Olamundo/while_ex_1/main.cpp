c33485bc7fbb3a3e37a8074d06ec627f37a36cab
Tiago Oliveira <tiagolivesan@outlook.com>
2021-01-28 12:03:38 -0300

    Exercicío While

---
+++ Olamundo/while_ex_1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int valor1, valor2;
+
+    cout << "Informe dois valores inteiros: ";
+
+    cin >> valor1 >> valor2;
+
+    while(valor1==valor2){
+        cout << "Informe dois valores inteiros: ";
+
+        cin >> valor1>> valor2;
+    }
+
+    cout << "Valores informados: " << valor1 << " " << valor2 << endl;
+
+    return 0;
+}
