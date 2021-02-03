00c439d152a860163b58eb471e03f6d964eab77e
Tiago Oliveira <tiagolivesan@outlook.com>
2021-02-03 16:38:33 -0300

    Funções

---
+++ Olamundo/funcoes/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x = 10.0;
+    double y = pow(x, 4.0);
+
+    cout << "x= " << x << " y=" << y << endl;
+
+    return 0;
+}
