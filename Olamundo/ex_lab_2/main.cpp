202234890f73eae752b15b36a6a36652fd600b0a
Tiago Oliveira <tiagolivesan@outlook.com>
2021-02-05 14:40:23 -0300

    ex_lab_2

---
+++ Olamundo/ex_lab_2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int pow(int b, int e) {
+
+}
+
+int main() {
+  int a, b, eq;
+  cin >> a >> b;
+
+  if(b>0){
+    eq = pow(a, b);
+  }
+
+
+  cout << eq;
+  return 0;
+}
