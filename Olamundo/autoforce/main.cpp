289b429a83f702f8dd7a4dd5d04f7e6dfd4c6e9a
Tiago Oliveira <tiagolivesan@outlook.com>
2021-04-25 12:57:22 -0300

    c++

---
+++ Olamundo/autoforce/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+
+#define MAX_TXT 100
+
+using namespace std;
+
+int main()
+{
+    int v[80];
+
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> v[i];
+    }
+    char numeros = v[80] + '0';
+    cout <<  numeros;
+
+}
